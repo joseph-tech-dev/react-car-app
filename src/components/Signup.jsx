bafc1afe86ce5930e68547acc6929c0e7cd7e1c3
isaacnyabuto <isaackombo822@gmail.com>
2025-02-26 12:56:38 +0300

    modified sign-up page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -31,6 +31,7 @@ const Signup = () => {
     };
 
     return (
+        
         <div className="auth-container">
             <h2>Sign Up</h2>
             <form id="signup-form" onSubmit={handleSubmit} encType="multipart/form-data">
@@ -48,6 +49,7 @@ const Signup = () => {
             </p>
             <p id="signup-message">{message}</p>
         </div>
+    
     );
 };
 
